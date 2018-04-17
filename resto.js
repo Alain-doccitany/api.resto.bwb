adca7193ca68cb6f0d5ceb67c168bfbbb4ca83df
alain doccitanie <alain@beweb-16.176A>
2018-04-17 19:24:12 +0200

    add get menu and cards add post (creat card) Signed-off-by:alain doccitanie <alain@beweb-16.176A> Signed-off-by:alain doccitanie <alain@beweb-16.176A>

--- resto.js
+++ resto.js
@@ -7,13 +7,13 @@
  */
 /*creat of variable for project*/
 
-/* global listeDeMenu */
+/* global listeDeCarte, response, menus, menu, listeDecarte */
 
 //Import du framework express
 
 var express = require("express");
 var bodyparser = require("body-parser");
-
+var listeDeCarte = [];
 
 //Creation d'un objet express
 
@@ -27,7 +27,9 @@ application.listen(8081,"127.0.0.1");
 application.use(bodyparser.json());
 application.use(bodyparser.urlencoded({extended : true}));
 
+var cartes = listeDeCarte;
 
+// Test connexion Test //ok
 application.get('/',
 
 	function(request,response){
@@ -40,9 +42,9 @@ application.get('/',
 
 
 
-//return liste des menus
+//return * cartes / postman ok
 
-application.get('/listeDeMenus',
+application.get('/cartes/get/',
 
 	function(request,response){
 
@@ -50,10 +52,115 @@ application.get('/listeDeMenus',
 
 		//Chainage reponse du serveur: status puis reponse
 
-		response.status(200).json(listeDeMenu);
+		response.status(200).json(listeDeCarte);
+                response.status(404).send(listeDeCarte);
 
 	}
-
+                                       
 );
 
 
+//return * menus /postman ok
+
+application.get('/menu/get/',
+
+	function(request,response){
+
+		response.setHeader('Access-Control-Allow-Origin','*');
+
+		//Chainage reponse du serveur: status puis reponse
+
+		response.status(200).json(listeDeCarte);
+                response.status(404).send(listeDeCarte);
+
+	}
+                                       
+);
+
+//ajout une carte et return son id /postman ok
+application.post("/cartes/add",function(req,res){
+    var carte = req.body;
+    carte ['id'] = generateID();
+    carte ['menus'] = [];
+    listeDeCarte.push(req.body);
+    res.status(200).json(req.body);
+
+});
+
+
+
+
+
+function getCarte(id){
+
+}
+//crée un id 
+function generateID(){
+    var idMax = 0;
+    for(var i in listeDeCarte){
+        idMax = (listeDeCarte[i].id > idMax)?listeDeCarte[i].id : idMax;
+}
+
+        return idMax +1;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//return une carte par son id
+application.get('/cartes/:id/get',
+
+	function(request,response){
+
+		response.setHeader('Access-Control-Allow-Origin','*');
+
+	
+                var idCartes = parseInt(request.params.id);
+
+		var carte;
+
+	
+                    for(var i = 0 ; i < listeDeCarte.length ; i++){
+
+			if(listeDeCarte[i].id === idCartes){
+
+				film = listeDeCarte[i];
+
+			}
+                }
+        //Chainage reponse du serveur: status puis reponse
+
+		response.status(200).json(carte);
+
+		response.status(404).send("Le menu n'existe pas ou n'est pas référencé");
+                }
+);
+//delete les menu par leur id
+application.delete("/carte/menu/:id",function(req,res){
+    var idDuMenu = req.params.id;
+    idDuMenu = parseInt(idDuMenu);
+
+    for(var i=0; i<listeDeCarte.length;i++){
+        if(idDuMenu === listeDeCarte[i].id){
+            listeDeCarte.splice(idDuMenu,1);
+            response.setHeader('Content-Type','application/json');
+}
+}
+response.status(200).json(listeDeCarte);
+});
+
+
+
+
+
+		
